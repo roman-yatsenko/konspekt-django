b31e543dd56013b4d143170ef8715b7a112f5fc0
YaRo <roman.yatsenko@hneu.net>
2026-05-23 17:18:25 +0300

    Add Note model and update Topic model options in konspekt app

--- konspekt/models.py
+++ konspekt/models.py
@@ -7,6 +7,23 @@ class Topic(models.Model):
     text = models.CharField(max_length=200, verbose_name="Назва теми")
     date_added = models.DateTimeField(auto_now_add=True, verbose_name="Створено")
 
+    class Meta:
+        verbose_name = "Тема"
+        verbose_name_plural ="Теми"
+    
     def __str__(self):
         return self.text
     
+class Note(models.Model):
+    """Нотатки за темами"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, verbose_name="Тема")
+    text = models.TextField(verbose_name="Текст")
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name="Створено")
+
+    class Meta:
+        verbose_name = "Нотатка"
+        verbose_name_plural = "Нотатки"
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
