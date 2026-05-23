3d44d210f9ed34171cd3e5954c2a35f9210d5860
YaRo <roman.yatsenko@hneu.net>
2026-05-23 16:29:55 +0300

    Add Topic model and initial migration for konspekt app

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'konspekt',
+
+    # Django applications
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
