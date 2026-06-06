c16cf7ec8ad2a4d478fc9f21984e1ffe5223221b
YaRo <roman.yatsenko@hneu.net>
2026-06-06 17:29:20 +0300

    Add topic detail view and template, update topics list to link to individual topics

--- konspekt/views.py
+++ konspekt/views.py
@@ -11,3 +11,10 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'konspekt/topics.html', context)
+
+def topic(request, topic_id):
+    """Виводе одну тему і всі її нотатки"""
+    topic = Topic.objects.get(id=topic_id)
+    notes = topic.note_set.order_by('-date_added')
+    context = {'topic': topic, 'notes': notes}
+    return render(request, 'konspekt/topic.html', context)
